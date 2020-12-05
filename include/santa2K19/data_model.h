5da0872dcb88cb3fdea425c5fb61038ae905fb48
Nuc94 <dan.dom.nuc@gmail.com>
2020-12-05 22:12:15 +0100

    written and tested families data parsing

--- include/santa2K19/data_model.h
+++ include/santa2K19/data_model.h
@@ -16,9 +16,27 @@ class DataModel
 {
 public:
     DataModel(const std::string & family_data_filename);
+    //method to obtain the chosen day for a given family and its choice
+    //if the family or the choice exceed the data ranges just return -1
+    inline int getFamilyChoice(const unsigned int family_id, const unsigned int choice) const {
+        if( family_id < this->families_choices.size()) {
+            if( choice < this->families_choices[family_id].size() ) {
+                return this->families_choices[family_id][choice];
+            }
+        }
+        return -1;
+    }
+    inline int getFamilyComponents(const unsigned int family_id) const {
+        if( family_id < this->families_choices.size()) {
+            return this->families_components[family_id];
+        }
+        return -1;
+    }
 private:
-    //matrix containing families data, one row for each family
-    std::array< std::array<int, N_CHOICES>, N_FAMILIES > families_data;
+    //matrix containing families choices, one row for each family
+    std::array< std::array<int, N_CHOICES>, N_FAMILIES > families_choices;
+    //array containing number of components for each family
+    std::array<int, N_FAMILIES> families_components;
     //internal method to parse families file
     void parseFamiliesCsv(std::ifstream & families_file);
 };
@@ -40,15 +58,20 @@ void DataModel::parseFamiliesCsv(std::ifstream & families_file) {
     std::getline(families_file, line);
     //NOW I HAVE TO WRITE CODE TO ACTUALLY GET ALL THE LINES IN A WHILE LOOP AND PARSE EACH ONE INTO THE ARRAY
     while( std::getline(families_file, line) ) {
-        //in this loop I read all lines
+        //in this loop I read all lines, for each of them I collect data related to the given family
         ss << line;
         std::getline(ss, acq, ',');
         family_id = std::stoi(acq);
         choice_level = 0;
-        while(std::getline(ss, acq, ',')) {
-            this->families_data[family_id][choice_level] = std::stoi(acq);
+        while(choice_level < N_CHOICES) {
+            std::getline(ss, acq, ',');
+            this->families_choices[family_id][choice_level] = std::stoi(acq);
             ++choice_level;
         }
+        std::getline(ss, acq);
+        this->families_components[family_id] = std::stoi(acq);
+        ss.str("");
+        ss.clear();
     }
 }
 
--- tests/datamodel_test.cpp
+++ tests/datamodel_test.cpp
@@ -8,3 +8,19 @@ const std::string DEFAULT_FILEPATH = "/home/nuc/Programmazione/Kaggle/Santa's Wo
 TEST(DATAMODEL_TEST, IniTest) {    
     DataModel DM(DEFAULT_FILEPATH);
 }
+
+TEST(DATAMODEL_TEST, CorrectFamilyChoiceTest) {    
+    DataModel DM(DEFAULT_FILEPATH);
+    ASSERT_EQ(DM.getFamilyChoice(0,0), 52);
+    ASSERT_EQ(DM.getFamilyChoice(0,9), 28);
+    ASSERT_EQ(DM.getFamilyChoice(4999,0), 13);
+    ASSERT_EQ(DM.getFamilyChoice(4999,9), 47);
+}
+
+TEST(DATAMODEL_TEST, CorrectFamilyComponentsTest) {    
+    DataModel DM(DEFAULT_FILEPATH);
+    ASSERT_EQ(DM.getFamilyComponents(0), 4);
+    ASSERT_EQ(DM.getFamilyComponents(1), 4);
+    ASSERT_EQ(DM.getFamilyComponents(4998), 5);
+    ASSERT_EQ(DM.getFamilyComponents(4999), 4);
+}
